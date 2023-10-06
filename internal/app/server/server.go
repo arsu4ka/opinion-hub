5389a3d332d79103fea9aab3ce99ca253b177d1b
Arsenii <origingoy@gmail.com>
2023-10-06 21:46:48 +0300

    added server struct and routes

--- internal/app/server/server.go
+++ internal/app/server/server.go
@@ -1,1 +1,50 @@
 package server
+
+import (
+	"github.com/aru4ka/opinion-hub/internal/app/configs"
+	"github.com/aru4ka/opinion-hub/internal/app/controllers"
+	"github.com/aru4ka/opinion-hub/internal/app/models"
+	"github.com/aru4ka/opinion-hub/internal/app/repositories"
+	"github.com/aru4ka/opinion-hub/internal/app/services"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"gorm.io/gorm"
+)
+
+type Server struct {
+	config *configs.ServerConfig
+	e      *echo.Echo
+}
+
+func New(config *configs.ServerConfig) *Server {
+	return &Server{config: config, e: echo.New()}
+}
+
+func (s *Server) init(db *gorm.DB) {
+	userRepo := repositories.NewGormUserRepository(db)
+
+	userService := services.NewUserService(userRepo)
+
+	authController := controllers.NewAuthController(userService)
+
+	s.e.Use(middleware.Recover())
+	s.e.Use(middleware.CORS())
+	s.e.Use(middleware.Logger())
+
+	authGroup := s.e.Group("/auth")
+	authGroup.POST("/signup", authController.SignUp())
+}
+
+func (s *Server) Start() error {
+	db, err := s.config.Db.GetDB()
+	if err != nil {
+		return err
+	}
+
+	if err := db.AutoMigrate(&models.User{}, &models.Opinion{}, &models.Like{}); err != nil {
+		return err
+	}
+
+	s.init(db)
+	return s.e.Start(":" + s.config.Port)
+}
