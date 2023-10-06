781e1542567f563592b9b0652f50ec4fd35112ef
Arsenii <origingoy@gmail.com>
2023-10-06 21:44:32 +0300

    main script for running app

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,1 +1,16 @@
 package main
+
+import (
+	"github.com/aru4ka/opinion-hub/internal/app/configs"
+	"github.com/aru4ka/opinion-hub/internal/app/server"
+)
+
+func main() {
+	config, err := configs.NewPostgresServerConfig(true)
+	if err != nil {
+		panic(err)
+	}
+
+	s := server.New(config)
+	panic(s.Start())
+}
