3d461db36bccebf4f7226a34a944385ebc739c00
Arsenii <origingoy@gmail.com>
2023-10-11 19:20:20 +0300

    claims&func for jwt generation

--- internal/app/utils/jwt.go
+++ internal/app/utils/jwt.go
@@ -1,1 +1,22 @@
 package utils
+
+import (
+	"github.com/golang-jwt/jwt/v5"
+	"time"
+)
+
+type JwtCustomClaims struct {
+	UserId uint `json:"userId"`
+	jwt.RegisteredClaims
+}
+
+func GenerateJWT(secret string, expirationHours int, userId uint) (string, error) {
+	claims := &JwtCustomClaims{
+		userId,
+		jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(time.Hour * time.Duration(expirationHours))),
+		},
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return token.SignedString([]byte(secret))
+}
